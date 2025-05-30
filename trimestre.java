3660d05ea8759ac6bc4cd3150ddcce8f0cbe8d9b
Enzyw <enzo.lima.sousa@aluno.edu.pi.gov.br>
2025-05-30 13:35:02 -0300

    Add files via upload

---
+++ trimestre.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;;
+public class trimestre {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ;
+        System.out.println("informe suas notas");
+        System.out.println("informe a nota 1");
+        double r1 = entrada.nextDouble();
+        System.out.println("informe a nota 2");
+        double r2 = entrada.nextDouble();
+        System.out.println("informe a nota 3");
+        double r3 = entrada.nextDouble();
+        System.out.println("informe a nota 4");
+        double r4 = entrada.nextDouble();
+        System.out.println("informe a nota 5");
+        System.out.println("informe a nota 6");
+        double r6 = entrada.nextDouble();
+        System.out.println("informe a nota 7");
+        double r7 = entrada.nextDouble();
+        System.out.println("informe a nota 8");
+        double r8 = entrada.nextDouble();
+
+        double jo = (r1 + r2) / 2;
+        double so = (r3 + r4) / 2;
+        double co = (jo + so) / 2;
+        double ao = (r5 + r6) / 2;
+        double io = (r7 + r8) /2;
+        double mo = (ao + io) /2;
+        double ro = ( r1+r2+r3+r4+r5+r6+r7+r8)/8;;
+        System
+
+
+
+    }
+
+}
+double r5 = entrada.nextDouble();
